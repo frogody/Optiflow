22685eececaf2ea1d7958c84f92d78d347924144
frogody <gody@isyncso.com>
2025-04-23 04:34:36 +0200

    fix: remove duplicate client code from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,37 +30,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
-
-'use client';
-
-import '@/styles/globals.css';
-import Navigation from '@/components/Navigation';
-import MicrophonePermission from '@/components/MicrophonePermission';
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import TanstackProvider from "@/components/providers/TanstackProvider";
-import { SessionProvider } from 'next-auth/react';
-import { Analytics } from '@vercel/analytics/react';
-import { Toaster } from 'react-hot-toast';
-
-function ClientProviders({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      <TanstackProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navigation />
-          <div id="microphone-permission-root" />
-          <MicrophonePermission />
-          {children}
-          <Toaster position="top-right" />
-          <Analytics />
-        </ThemeProvider>
-      </TanstackProvider>
-    </SessionProvider>
-  );
 } 
