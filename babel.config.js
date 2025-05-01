e958f97b937e260a487b927fec7170e3fa35bbd6
frogody <gody@isyncso.com>
2025-05-01 03:00:33 +0200

    build: remove babel config to use Next.js SWC compiler

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ["next/babel"],
-  plugins: [
-    "@babel/plugin-transform-private-methods",
-    "@babel/plugin-transform-private-property-in-object",
-    "@babel/plugin-transform-class-properties"
-  ]
-};
