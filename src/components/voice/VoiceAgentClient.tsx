f85b90624e81fbae79451b507fc44fbb84e764f6
frogody <gody@isyncso.com>
2025-05-09 06:23:18 +0200

    refactor(voice): move voice agent client to proper Next.js component location

---
+++ src/components/voice/VoiceAgentClient.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource react */
+'use client';
+
+import React from 'react';
+import type { FC } from 'react';
+
+interface VoiceAgentClientProps {}
+
+const VoiceAgentClient: FC<VoiceAgentClientProps> = () => {
+  return React.createElement('div', null, 'Voice Agent Widget Placeholder');
+};
+
+export default VoiceAgentClient; 
