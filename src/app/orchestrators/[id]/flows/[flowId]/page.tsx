d24c62f623eea58d9637cc92e72810cd6e821466
frogody <gody@isyncso.com>
2025-05-10 22:28:15 +0200

    Use relative paths in FlowPage to test alias resolution

--- src/app/orchestrators/[id]/flows/[flowId]/page.tsx
+++ src/app/orchestrators/[id]/flows/[flowId]/page.tsx
@@ -3,8 +3,8 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-import FlowDashboard from '@/components/FlowDashboard.js';
-import { useUserStore } from '@/lib/userStore.js';
+import FlowDashboard from '../../../../../components/FlowDashboard.js';
+import { useUserStore } from '../../../../../lib/userStore.js';
 
 export default function FlowPage({ params }: { params: { id: string; flowId: string     } }): React.ReactNode {
   const router = useRouter();
