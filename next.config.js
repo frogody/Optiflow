d33343dbe7c4a3a6a242fab841ad81b6655314ac
frogody <gody@isyncso.com>
2025-05-10 02:48:26 +0200

    Simplify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
   images: {
     remotePatterns: [
@@ -13,7 +12,6 @@ const nextConfig = {
       }
     ],
     dangerouslyAllowSVG: true,
-    unoptimized: false,
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'ui-avatars.com'],
   },
   typescript: {
@@ -22,19 +20,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  trailingSlash: true,
   env: {
     NEXT_PUBLIC_DEPLOYMENT_ENV: process.env.NODE_ENV || 'development',
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXT_PUBLIC_PIPEDREAM_CLIENT_ID: process.env.NEXT_PUBLIC_PIPEDREAM_CLIENT_ID || 'kWYR9dn6Vmk7MnLuVfoXx4jsedOcp83vBg6st3rWuiM',
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'https://app.isyncso.com',
     NEXT_PUBLIC_PIPEDREAM_REDIRECT_URI: process.env.NEXT_PUBLIC_PIPEDREAM_REDIRECT_URI || 'https://app.isyncso.com/api/pipedream/callback',
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  },
-  i18n,
-  transpilePackages: ['next'],
-  experimental: {
-    forceSwcTransforms: false
   }
 }
 
